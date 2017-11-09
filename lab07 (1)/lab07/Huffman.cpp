587187123885b6663210ca34564fe482e47c32ce
Kylan Strope <kylanstrope@gmail.com>
2017-11-09 06:45:54 +0000

    ping

--- lab07 (1)/lab07/Huffman.cpp
+++ lab07 (1)/lab07/Huffman.cpp
@@ -1,150 +1,97 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <queue>
+
 using namespace std;
 
-struct Node {
-  int freq;
-  int id;
-  Node *left;
-  Node *right;
+class Node {
+	public:
+	int root; //1 if root, 0 if not
+	int id;
+	int freq;
+	Node* left;
+	Node* right;
+	Node();
+	~Node();
+	Node (int check, int ident, int frequency);
 };
 
-class MinHeap {
+Node::Node(){
+};
 
-  Node **set;
-  int max_size;
-  int current_size;
-
-public:
-  MinHeap(int size);
-
-  int parent(int i) {
-    return (i-1)/2;
-  }
-
-  int left(int i) {
-    return 2*i+1;
-  }
-
-  int right (int i) {
-    return 2*i+2;
-  }
-
-  Node* extractMin();
-  void minHeapify(int i);
-  void insert(Node *j);
+Node::Node(int check, int ident, int frequency) {
+	root = check;
+	id = ident;
+	freq = frequency;
+	left = NULL;
+	right = NULL;
 }
 
-  MinHeap::MinHeap(int size) {
-    current_size = 0;
-    max_size = size;
-    set = new int[size];
-  }
+struct comp : public binary_function<Node*, Node*, bool> {
+	bool operator() (const Node* l, const Node* r) const{
+		return (l->freq > r->freq);
+	}
+};
 
-Node* MinHeap::extractMin() {
-  if (current_size <= 0)
-    return 0;
-  if (current_size == 1) {
-    current_size--;
-    return set[0];
-  }
-  Node* root = set[0];
-  set[0] = set[current_size-1];
-  current_size--;
-  minHeapify(0);
-  return root;
-}
-
-
-
-void MinHeap::minHeapify(int i) {
-  int x = left(i);
-  int y = right(i);
-  int z = i;
-  Node* temp = new Node;
-  if (x < current_size && set[x]->freq < set[i]->freq) {
-    z = x;
-  }
-  if (y < current_size && set[y]->freq < set[z]->freq) {
-    z = y;
-  }
-  if (z != i) {
-    temp = set[i];
-    set[i] = set[z];
-    set[z] = temp;
-    MinHeapify(z);
-  }
-}
-
-void MinHeap::insert(Node *j) {
-  Node* temp = new Node;
-  current_size++;
-  int check = current_size - 1;
-  set[check] = j;
-
-  while (check != 0 && set[parent(check)]->freq > set[check]->freq) {
-    temp = set[check];
-    set[check] = set[parent(check)];
-    set[parent(check)] = temp;
-    check = parent(check);
-  }
-}
-
-void print(Node* Q, string code){
-	if (!Q) {
+void print(Node* input, string code, int id) {
+	//cout << "debug1" << endl;
+	if (input == NULL) {
+		//cout << "debug2" << endl;
 		return;
 	}
-	cout << code;
-	print(Q->left, code + "0");
-	print(Q->right, code + "1");
-	cout << endl;
-}
-
-void Huffman (MinHeap Q, int size) {
-  Node* x = new Node;
-  Node* y = new Node;
-  Node* z = new Node;
-  for (int i = 0; i < n-1; i++) {
-    x = Q.extractMin();
-	y = Q.extractMin();
-	if (x->freq == y->freq) {
-		if (x->id < y->id) {
-			z->left = x;
-			z->right = y;
-		}
-		else if (y->id < x->id) {
-			z->left = y;
-			z->right = x;
-		}
+	else if (input->root != 1 && id == input->id) {
+		//cout << "debug3" << endl;
+		cout << code << endl;
 	}
-    z->freq = x->freq + y->freq;
-    z->id = x->id;
-    Q.insert(z);
-  }
-  print (Q.extractMin(), "");
-  
+	//cout << "debug6" << endl;
+	print(input->left, code+"0", id);
+	//cout << "debug4" << endl;
+	print(input->right, code+"1", id);
+	//cout << "debug5" << endl;
 }
 
+void Huffman (int id[], int freq[], int size) {
+	Node* left = new Node;
+	Node* right = new Node;
+	Node* root = new Node;
+	Node* temp = new Node;
+	int newID;
+	priority_queue<Node*, vector<Node*>, comp> minHeap;
+	for (int i = 0; i < size; i++) {
+		minHeap.push(new Node(0, id[i], freq[i]));
+	}
+	while (minHeap.size() != 1) {
+		left = minHeap.top();
+		minHeap.pop();
+		right = minHeap.top();
+		minHeap.pop();
+		newID = left->id;
+		if (left->freq == right->freq) {
+			if (right->id < left->id) {
+				temp = right;
+				right = left;
+				left = temp;
+				newID = right->id;
+			}
+		}
+		root = new Node(1, newID, left->freq+right->freq);
+		root->left = left;
+		root->right = right;
+		minHeap.push(root);
+	}
+	for (int i = 0; i < size; i++) {
+		print(minHeap.top(), "", id[i]);
+	}
+}
 
-
-int main (){
-  int num;
-  int id = 0;
-  cin >> num;
-  int input;
-  MinHeap Q(num);
-  for (int i = 0; i < num; i++) {
-    Node* temp = new Node;
-    cin >> input;
-    temp->freq = input;
-    temp->id = id;
-    temp->left = NULL;
-    temp->right = NULL;
-    Q.insert(temp);
-    id++;
-  }
-  
-  Huffman(Q, num);
-  return 0;
+int main() {
+	int size;
+	cin >> size;
+	int freq[size];
+	int id[size];
+	for (int i = 0; i < size; i++) {
+		cin >> freq[i];
+		id[i] = i;
+	}
+	Huffman(id, freq, size);
 }
